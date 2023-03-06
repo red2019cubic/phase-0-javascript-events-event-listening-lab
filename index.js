bd3e5d7047daef471589915202cc6e458dea0573
Red <red.sarouf2014@gmail.com>
2023-03-06 02:55:29 -0500

    Added JavaScript Event Listeners Lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
 }
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
